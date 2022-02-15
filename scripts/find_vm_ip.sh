9298d34ebc663098af86264661629f7d790a2324
morgan guy <morganguy@morgans-MacBook-Pro.local>
2022-02-15 19:17:32 +0100

    walkthrough not finished

--- scripts/find_vm_ip.sh
+++ scripts/find_vm_ip.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-VBoxManage guestproperty get Boot2Root "/VirtualBox/GuestInfo/Net/0/V4/IP"
+vBoxManage guestproperty enumerate boot2beroot
+VBoxManage guestproperty get boot2beroot "/VirtualBox/GuestInfo/Net/0/V4/IP"
